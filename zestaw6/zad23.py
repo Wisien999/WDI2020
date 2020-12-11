e7c274a65d77537439c9837fad0d35f10cd954d4
Wisien <wisnia3221@gmail.com>
2020-12-11 15:40:02 +0100

    Fix 23

--- zestaw6/zad23.py
+++ zestaw6/zad23.py
@@ -3,25 +3,52 @@
 # całkowitej liczbie kΩ) łącząc dowolnie 3 wybrane rezystory.
 
 
+def poss(t, idx, target):
+    if abs(t[idx[0]] + t[idx[1]] + t[idx[2]] - target) < 0.00001:
+        return True
+    if abs((t[idx[0]] * t[idx[1]] * t[idx[2]])/(t[idx[0]]*t[idx[2]] + t[idx[1]]*t[idx[2]] + t[idx[2]]*t[idx[1]]) - target) < 0.00001:
+        return True
+    if abs(t[idx[0]] + (t[idx[1]] * t[idx[2]])/(t[idx[1]] + t[idx[2]]) - target) < 0.00001:
+        return True
+    if abs(t[idx[2]] + (t[idx[1]] * t[idx[0]])/(t[idx[1]] + t[idx[0]]) - target) < 0.00001:
+        return True
+    if abs(t[idx[0]] + (t[idx[1]] * t[idx[2]])/(t[idx[1]] + t[idx[2]]) - target) < 0.00001:
+        return True
+    
+    x = t[idx[0]] + t[idx[1]]
+    if abs((x * t[idx[2]])/(x + t[idx[2]]) - target) < 0.00001:
+        return True
+
+    x = t[idx[0]] + t[idx[2]]
+    if abs((x * t[idx[1]])/(x + t[idx[1]]) - target) < 0.00001:
+        return True
+
+    x = t[idx[1]] + t[idx[2]]
+    if abs((x * t[idx[0]])/(x + t[idx[0]]) - target) < 0.00001:
+        return True
+    
+
+    return False
+
+
 def zad(t, target):
-    def rek(t, target, index=0, resistance=None, no_of_resistors=1, h=None):
-        resistance = resistance or t[0]
-        h = h or [(t[0], "first")]
+    def rek(t, target, index=0, idx=None, no_of_resistors=0):
+        idx = idx or []
+        # h = h or [(t[0], "first")]
 
         if no_of_resistors == 3:
-            print(resistance, h, no_of_resistors)
-            return abs(resistance - target) < 0.0001
+            # print(resistance, h, no_of_resistors)
+            return poss(t, idx, target)
 
         if index == len(t):
             return False
         
         
-        return rek(t, target, index+1, resistance + t[index], no_of_resistors+1, [*h, (t[index], "szereg")]) or \
-                rek(t, target, index+1, resistance, no_of_resistors, [*h]) or \
-                rek(t, target, index+1, (resistance*t[index])/(resistance+t[index]), no_of_resistors+1, [*h, (t[index], "pararel")])
+        return rek(t, target, index+1, [*idx, index], no_of_resistors+1) or \
+                rek(t, target, index+1, [*idx], no_of_resistors)
 
-    return rek(t, target, 0, t[0], 1, [(t[0], "first")]) or \
-        rek(t, target, 1, t[1], 1, [(t[1], "first")])
+    return rek(t, target, 0, [0], 1) or \
+        rek(t, target, 1, [1], 1)
 
 
 res = [3,6,2,4,2,6,2]
