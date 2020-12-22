3176546623738119e6fe9752a52f004e276b3048
Wisien <wisnia3221@gmail.com>
2020-12-22 09:49:23 +0100

    Optimize

--- zestaw6/zad15.py
+++ zestaw6/zad15.py
@@ -24,19 +24,25 @@ def rek(n, left, x_tab, y_tab, ld_tab, rd_tab, history=[]):
     for x in range(n):
         for y in range(n):
             if not x_tab[x] and not y_tab[y] and not ld_tab[convert2ld(x, y)] and not rd_tab[convert2rd(x, y)]:
-                x_tab_c = x_tab.copy()
-                y_tab_c = y_tab.copy()
-                ld_tab_c = ld_tab.copy()
-                rd_tab_c = rd_tab.copy()
+                # x_tab_c = x_tab.copy()
+                # y_tab_c = y_tab.copy()
+                # ld_tab_c = ld_tab.copy()
+                # rd_tab_c = rd_tab.copy()
 
-                x_tab_c[x] = True
-                y_tab_c[y] = True
-                ld_tab_c[convert2ld(x, y)] = True
-                rd_tab_c[convert2rd(x, y)] = True
+                x_tab[x] = True
+                y_tab[y] = True
+                ld_tab[convert2ld(x, y)] = True
+                rd_tab[convert2rd(x, y)] = True
 
-                if rek(n, left-1, x_tab_c, y_tab_c, ld_tab_c, rd_tab_c, [*history, (x,y)]):
+                if rek(n, left-1, x_tab, y_tab, ld_tab, rd_tab, [*history, (x,y)]):
                     return True
                 
+                
+                x_tab[x] = False
+                y_tab[y] = False
+                ld_tab[convert2ld(x, y)] = False
+                rd_tab[convert2rd(x, y)] = False
+
     return False
 
 pos = []
