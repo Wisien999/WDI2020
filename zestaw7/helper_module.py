44a928452ec25586a36651d0bbadfd3052f27050
Wisien <wisnia3221@gmail.com>
2020-12-25 00:17:45 +0100

    Change reverse function

--- zestaw7/helper_module.py
+++ zestaw7/helper_module.py
@@ -5,12 +5,31 @@ class Node:
         self.next = None
 
 
+class Node_d:
+    def __init__(self, value = None):
+        self.val = value
+        self.next = None
+        self.prev = None
+
+
 def print_ll(ll: Node):
     c = ll
     while c != None:
         # if c.val =
         print(c.val, end="     ")
         c = c.next
+    print()
+
+
+def print_back_ll(ll: Node):
+    c = ll
+    while c.next != None:
+        c = c.next
+
+    while c != None:
+        print(c.val, end="      ")
+        c = c.prev
+
     print()
 
 
@@ -35,16 +54,46 @@ def arr2list(arr):
     return ans
 
 
+def arr2dlist(arr):
+    ans = Node_d()
+    c = ans
+
+    for el in arr:
+        c.next = Node_d(el)
+        # c = c.next
+        c.next.prev = c
+        c = c.next    
+    
+    return ans
+
+
 
 def reverse_list(l: Node, ans: Node):
     """
     Reverses given list l and adds it to ans.
-    "ans" must be Node oject
+    "ans" must be Node object
     """
-    
-    if l.next != None:
-        reverse_list(l.next, ans)
-    
-    if l.val != None:
-        push_back(ans, l.val)
 
+    if l.val == None:
+        l = l.next
+
+    while l != None:
+        # if l.val == None:
+        push_front(ans, l.val)
+        l = l.next
+    
+
+    return ans
+            
+    
+    # if l.next != None:
+    #     reverse_list(l.next, ans)
+    
+    # if l.val != None:
+    #     push_back(ans, l.val)
+
+
+def push_front(l, val):
+    q = Node(val)
+    q.next = l.next
+    l.next = q
