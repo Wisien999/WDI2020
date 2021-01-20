af39994446754a6a166a09d9e445680e16403f1d
Wisien <wisnia3221@gmail.com>
2021-01-20 21:43:13 +0100

    Fix bug

--- extra/kartkowki/2016-2017/zad1.py
+++ extra/kartkowki/2016-2017/zad1.py
@@ -2,22 +2,29 @@ def solve(k):
     a, b = 1, 2
     la, lb = 1, 2
     i = 1
+    bestA, bestB = -1, -1
+    Ai, Bi = 1, 1
 
-    while True:
-        # print(a, b)
-        if abs(k-la) < abs(k-a):
-            print("ciąg A nr", i-1)
-            return 
+    while bestA < 0 or bestB < 0:
+        print(i, " | ", a, b)
+        if bestA < 0 and abs(k-la) < abs(k-a):
+            bestA = abs(k-la)
+            Ai = i-1
 
-        if abs(k-lb) < abs(k-b):
-            print("ciąg B nr", i-1)
-            return 
+        if bestB < 0 and abs(k-lb) < abs(k-b):
+            bestB = abs(k-lb)
+            Bi = i-1
 
 
         la, lb = a, b
         b = b+a
         a = a + b/3
         i += 1
+
+    if bestA < bestB:
+        print("ciąg A nr", Ai)
+    else:
+        print("ciąg B nr", Bi)
 
 
 solve(int(input()))
